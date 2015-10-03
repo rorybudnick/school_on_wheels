a9ba76f1cacf4442e0d3ea84ad9211b6380d13a4
Rory Budnick <rorybudnick@gmail.com>
2015-10-02 23:02:48 -0700

    add to tutors controller, add session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,30 @@
+class SessionController < ApplicationController
+  def new
+    render(:new)
+  end
+
+  def create
+    # find if any user has that email
+    tutor = Tutor.find_by(email: params[:email])
+    
+    # if that user exists and it has a password that was sent
+    if tutor && (tutor.authenticate(params[:password]))
+
+      # save the user_id in the session hash
+      session[:tutor_id] = tutor.id
+
+      # and redirect to that user's home page
+      redirect_to(tutor_path(tutor))
+    else
+
+      # the email/password is wrong!
+      @message = "This email and password combination does not exist."
+      render(:new)
+    end
+  end
+
+  def destroy
+    session[:tutor_id] = nil
+    redirect_to root_path
+  end
+end
--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,16 +1,67 @@
 class TutorsController < ApplicationController
+  before_action :load_tutor, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, :authorize, only: [:show, :edit, :update]
+
   def new
     @tutor = Tutor.new
   end
 
+  def create
+    @tutor = Tutor.new(tutor_params)
+    if @tutor.save
+      session[:tutor_id] = @tutor.id
+      redirect_to tutor_path(@tutor)
+    else
+      render(:new)
+    end
+  end
+
+  def update
+    @update_worked = @tutor.update(tutor_params)
+    if @update_worked
+      redirect_to tutor_path(@tutor)
+    else
+      render(:edit)
+    end
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+    @update_worked = true
+  end
+
+  def destroy
+    @tutor.destroy
+    session.destroy
+    redirect_to root_path
+  end
+
   private
 
-  def load_user
+  def load_tutor
     return @tutor = Tutor.find(params[:id])
   end
 
-  def user_params
-    params.require(:tutor).permit(:first_name, :last_name, :email_address, :password, :password_confirmation, :phone_number, :address)
+  def tutor_params
+    params.require(:tutor).permit(
+      :first_name,
+      :last_name,
+      :email_address,
+      :password,
+      :password_confirmation,
+      :phone_number,
+      :address,
+      :interviewed,
+      :grades,
+      :notes,
+      :matched,
+      :dv
+    )
   end
 
   def authenticate
@@ -20,7 +71,7 @@ class TutorsController < ApplicationController
   end
 
   def authorize
-    unless current_user == @user
+    unless current_user == @tutor
       redirect_to login_path
     end
   end
